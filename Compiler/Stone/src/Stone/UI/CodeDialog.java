f9a0bc382848f67b3eec46f8ab9c4cf004c1d7fc
njustbyq <2042675095@qq.com>
2021-12-27 22:43:14 +0800

    UI

--- Compiler/Stone/src/Stone/UI/CodeDialog.java
+++ Compiler/Stone/src/Stone/UI/CodeDialog.java
@@ -12,5 +12,13 @@ public class CodeDialog extends Reader{
     private String buffer = null;
     private int pos = 0;
 
+    public static Reader file() throws FileNotFoundException {
+        JFileChooser chooser = new JFileChooser();
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            return new BufferedReader(new FileReader(chooser.getSelectedFile()));
+        } else {
+            throw new FileNotFoundException("no file specified");
+        }
+    }
     
 }

8b88d08d6762243bba10d28539e1d68a50d091f9
njustbyq <2042675095@qq.com>
2022-01-14 17:58:16 +0800

    update Parser

--- Compiler/Stone/src/Stone/Parser/Parser.java
+++ Compiler/Stone/src/Stone/Parser/Parser.java
@@ -3,3 +3,28 @@ package Stone.Parser
 import Stone.AST.ASTLeaf;
 import Stone.AST.ASTList;
 import Stone.AST.ASTree;
+import Stone.Exception.ParseException;
+import Stone.Lexer;
+import Stone.Token.Token;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
+
+/**
+ * A parser for grammatical analysis
+ */
+
+ public class Parser {
+     protected static abstract class Element {
+         protected abstract void parse(Lexer lexer, List<ASTree> res)
+                throws ParseException;
+         
+         protected abstract boolean match(Lexer lexer) throws ParseException;
+     }
+
+     
+ }

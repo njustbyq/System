0b1e19242f44f1b3e46ddbd8c4d682379ee6b4c6
njustbyq <2042675095@qq.com>
2022-01-18 18:58:51 +0800

    update Parser

--- Compiler/Stone/src/Stone/Parser/Parser.java
+++ Compiler/Stone/src/Stone/Parser/Parser.java
@@ -25,4 +25,59 @@ public class Parser {
          
         protected abstract boolean match(Lexer lexer) throws ParseException;
     }
+
+    protected static class Tree extends Element {
+        protected Parser parser;
+
+        protected Tree(Parser p) {
+            parser = p;
+        }
+
+        protected void parse(Lexer lexer, List<ASTree> res)
+                throws ParseException {
+            res.add(parser.parse(lexer));
+        }
+
+        protected boolean match(Lexer lexer) throws ParseException {
+            return parser.match(lexer);
+        }
+    }
+
+    protected static class OrTree extends Element {
+        protected Parser[] parsers;
+
+        protected OrTree(Parser[] p) {
+            parsers = p;
+        }
+
+        protected void parse(Lexer lexer, List<ASTree> res) 
+                throws ParseException {
+            Parser p = choose(lexer);
+            if (p == null) 
+                throw new ParseException(lexer.peek(0));
+            else    
+                res.add(p.parse(lexer));
+        }
+
+        protected boolean match(Lexer lexer) throws ParseException {
+            return choose(lexer) != null;
+        }
+
+        protected Parser choose(Lexer lexer) throws ParseException {
+            for (Parser p : parsers)
+                if (p.match(lexer))
+                    return p;
+            
+            return null;
+        }
+
+        protected void insert(Parser p) {
+            Parser[] newParsers = new Parser[parsers.length + 1];
+            newParsers[0] = p;
+            System.arraycopy(parsers, 0, newParsers, 1, parsers.length);
+            parsers = newParsers;
+        }
+    }
+
+    
 }    

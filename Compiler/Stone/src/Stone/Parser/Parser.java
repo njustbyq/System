69bf34c3e77dc57e11071f4c1da85dba6bd36e06
njustbyq <2042675095@qq.com>
2022-01-19 21:16:13 +0800

    update Parser

--- Compiler/Stone/src/Stone/Parser/Parser.java
+++ Compiler/Stone/src/Stone/Parser/Parser.java
@@ -12,6 +12,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.reflect.Method;
+import java.lang.invoke.ClassSpecializer.Factory;
 import java.lang.reflect.Constructor;
 
 /**
@@ -79,5 +80,80 @@ public class Parser {
         }
     }
 
+    protected static class Repeat extends Element {
+        protected Parser parser;
+        protected boolean onlyOnce;
+
+        protected Repeat(Parser p, boolean once) {
+            parser = p;
+            onlyOnce = once;
+        }
+
+        protected void parse(Lexer lexer, List<Pava.AST.ASTree> res)
+                throws ParseException {
+            while (parser.match(lexer)) {
+                Pava.AST.ASTree t = parser.parse(lexer);
+                if (t.getClass() != ASTList.class || t.numChildren() > 0)
+                    res.add(t);
+                if (onlyOnce)
+                    break;
+            }
+        }
+
+        protected boolean match(Lexer lexer) throws ParseException {
+            return parser.match(lexer);
+        }
+    }
+
+    protected static abstract class AToken extends Element {
+        protected Factory factory;
+
+        protected AToken(Class<? extends ASTLeaf> type) {
+            if (type == null)
+                type = ASTLeaf.class;
+            factory = Factory.get(type, Token.class);
+        }
+
+        protected void parse(Lexer lexer, List<ASTLeaf> res)
+                throws ParseException {
+            Token t = lexer.read();
+            if (test(t)) {
+                ASTLeaf leaf = factory.make(t);
+                res.add(leaf);
+            } else
+                throw new ParseException(t);
+        }
+
+        protected boolean match(Lexer lexer) throws ParseException {
+            return test(lexer.peek(0));
+        }
+
+        protected abstract boolean test(Token t);
+    }
+
+    protected static class IdToken extends AToken {
+        HashSet<String> reserved;
+
+        protected IdToken(Class<? extends ASTLeaf> type, HashSet<String> r) {
+            super(type);
+            reserved = r != null ? r : new HashSet<String>();
+        }
+
+        protected boolean test(Pava.Token.Token t) {
+            return t.isIdentifier() && !reserved.contains(t.getText());
+        }
+    }
+
+    protected static class NumToken extends AToken {
+        protected NumToken(Class<? extends ASTLeaf> type) {
+            super(type);
+        }
+
+        protected boolean test(Pava.Token.Token t) {
+            return t.isNumber();
+        }     
+    }
     
+    
+
 }    

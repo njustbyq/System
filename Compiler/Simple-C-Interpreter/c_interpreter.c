e6fcf73fa417bf7cb524286d8dd8856593d28700
njustbyq <2042675095@qq.com>
2023-04-19 23:39:28 +0800

    Simulate code seg, data seg and stack

--- Compiler/Simple-C-Interpreter/c_interpreter.c
+++ Compiler/Simple-C-Interpreter/c_interpreter.c
@@ -3,10 +3,14 @@
 #include <memory.h>
 #include <string.h>
 
-int token;              // current token
-char *src, *old_src;    // pointer to source code string
-int poolsize;           // default size of text/data stack
-int line;               // line number
+int  token;              // current token
+char *src, *old_src;     // pointer to source code string
+int  poolsize;           // default size of text/data stack
+int  line;               // line number
+int  *text,              // text segment
+     *old_text,          // for dump text segment
+     *stack;             // stack
+char *data;              // data segment
 
 void next() {
     token = *src++;
@@ -56,6 +60,26 @@ int main(int argc, char **argv)
     }
     src[i] = 0; // add EOF character
     close(fd);
+
+    // allocate memory for virtual machine
+    if (!(text = old_text = malloc(poolsize))) {
+        printf("could not malloc(%d) for text area\n", poolsize);
+        return -1;
+    }
+
+    if (!(data = malloc(poolsize))) {
+        printf("could not malloc(%d) for data area\n", poolsize);
+        return -1;
+    }
+
+    if (!(stack = malloc(poolsize))) {
+        printf("could not malloc(%d) for data area\n", poolsize);
+        return -1;
+    }
+
+    memset(text, 0, poolsize);
+    memset(data, 0, poolsize);
+    memset(stack, 0, poolsize);
 
     program();
     return eval();
